61d1bd7ee6c9a32ff9e989be146b85354a0f8183
Albert <albertxgm@gmail.com>
2023-02-02 16:47:37 +0300

    Авторизация и генерация JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,17 +3,28 @@ import { AuthController } from './auth.controller';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { AuthService } from './auth.service';
 import { UserModel } from './user.model';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getJwtConfig } from '../configs/jwt.config';
 
 @Module({
   controllers: [AuthController],
-  imports: [TypegooseModule.forFeature([
-    {
-      typegooseClass: UserModel,
-      schemaOptions: {
-        collection: 'User'
-      }
-    }
-  ])],
-  providers: [AuthService]
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: UserModel,
+        schemaOptions: {
+          collection: 'User',
+        },
+      },
+    ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJwtConfig
+    }),
+  ],
+  providers: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule {
+}
