59ff001953c348168cd10454cfac6fe40b413b9f
Albert <albertxgm@gmail.com>
2023-02-02 17:26:12 +0300

    Декоратор для получения пользователя

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -14,6 +14,7 @@ import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { UserEmail } from '../decorators/user-email.decorator';
 
 @Controller('review')
 export class ReviewController {
@@ -34,8 +35,9 @@ export class ReviewController {
     }
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('byProduct/:productId')
-  async getByProduct(@Param('productId') productId: string) {
+  async getByProduct(@Param('productId') productId: string, @UserEmail() email: string) {
     return this.reviewService.findByProductId(productId);
   }
 
